5175aa1bba15e47ec3c400f270229ed5a6dc6148
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-09-23 00:32:20 +0300

    Add js.

---
+++ GoIT/markup/home/exam/js/script.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+
+    $('#lakehouse').hide();
+    $('#tab-wrap2').hide();
+    $('#tab-wrap3').hide();
+
+    // Slider
+    $('.movie-image-ad-link').click(function() {
+        $('.movie-logo').hide();
+        $('#lakehouse').show();
+    });
+
+    $('.movie-image-ad-right').click(function() {
+        $('#lakehouse').hide();
+        $('.movie-logo').show();
+    });
+
+    $('.movie-image-ad-left').click(function() {
+        $('#lakehouse').hide();
+        $('.movie-logo').show();
+    });
+
+    // Tabs
+    $('#tab1').click(function() {
+        $('#tab1').addClass('active-tab');
+        $('#tab2').removeClass('active-tab');
+        $('#tab3').removeClass('active-tab');
+        $('#tab-wrap1').show();
+        $('#tab-wrap2').hide();
+        $('#tab-wrap3').hide();
+    });
+
+    $('#tab2').click(function() {
+        $('#tab2').addClass('active-tab');
+        $('#tab1').removeClass('active-tab');
+        $('#tab3').removeClass('active-tab');
+        $('#tab-wrap2').show();
+        $('#tab-wrap1').hide();
+        $('#tab-wrap3').hide();
+    });
+
+    $('#tab3').click(function() {
+        $('#tab3').addClass('active-tab');
+        $('#tab1').removeClass('active-tab');
+        $('#tab2').removeClass('active-tab');
+        $('#tab-wrap3').show();
+        $('#tab-wrap1').hide();
+        $('#tab-wrap2').hide();
+    });
+
+});
