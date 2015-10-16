ab77156eedeed5b758901d0df0ea557fe4973017
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-10-16 22:41:46 +0300

    Add lesson6 tasks 3 and 4

---
+++ GoIT/javascript/home/lesson6/task4.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Solovyov on 16.10.2015.
+ */
+var vasya = { name: 'Вася', age: 23 };
+var masha = { name: 'Маша', age: 18 };
+var vovochka = { name: 'Вовочка', age: 6 };
+
+var people = [ vasya , masha , vovochka ];
+
+
+
+console.log(people[0].age);
