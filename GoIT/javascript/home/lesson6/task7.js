f9b4f03da8801771f6b307377384affed064ace3
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-10-20 00:58:53 +0300

    Add lesson6 task7 hw

--- GoIT/javascript/home/lesson6/task7.js
+++ GoIT/javascript/home/lesson6/task7.js
@@ -2,22 +2,25 @@
  * Created by Solovyov on 17.10.2015.
  */
 var anagrams = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
-var newArr = anagrams.join().toLowerCase().split(',');
 
-newArr.sort(function (a, b) {
-   if (a.length < b.length) {
-       return -1;
-   }
-   if (a.length > b.length) {
-       return 1;
-   }
-   return 0;
-});
-
-
-console.log(newArr);
-/*console.log(anClean(anagrams));
+console.log(anClean(anagrams));
 
 function anClean(arr) {
 
-} */
+    var obj = {};
+    var newArr;
+
+    for (var i = 0; i < arr.length; i++) {
+        newArr = arr[i].toLowerCase().split('').sort().join('');
+        obj[newArr] = arr[i];
+    }
+
+    var result = [];
+
+    for (var key in obj) {
+        result.push(obj[key]);
+    }
+
+    return result;
+
+}
