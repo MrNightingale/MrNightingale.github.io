cf1f0cb7ee08d534c301de69758a058d59fc1061
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-10-25 23:03:37 +0200

    fix in the lesson7 codewars 1 and 3 tasks

--- GoIT/javascript/home/lesson7/codewars1.js
+++ GoIT/javascript/home/lesson7/codewars1.js
@@ -3,40 +3,21 @@
  */
 function createFunctions(n) {
     var callbacks = [];
-    var j = 0;
+
     for (var i=0; i<n; i++) {
+        // use anonymous wrapper to get right value of i
+        // http://bonsaiden.github.io/JavaScript-Garden/#function.closures
+        (function(e) {
+            callbacks.push(function() {
+                return e;
+            });
+        })(i);
 
-        callbacks.push(function () {
-
-            return j++;
-
-        });
-        console.log(j);
     }
 
     return callbacks;
 }
 
-//var callback = createFunctions(5);
+var callback = createFunctions(5);
 
-console.log(createFunctions(5)[1]());
-
-/*function createFunctions(n) {
-    var callbacks = [];
-
-    return function () {
-        for (var i=0; i<n; i++) {
-            callbacks.push(function() {
-
-            });
-            return i;
-
-        }
-    };
-
-
-}
-
-var callbacks1 = createFunctions(5);
-
-console.log(callbacks1[5]()); */
+console.log(callback[4]());
--- GoIT/javascript/home/lesson7/codewars3.js
+++ GoIT/javascript/home/lesson7/codewars3.js
@@ -2,18 +2,16 @@
  * Created by Solovyov on 23.10.2015.
  */
 var make_lazy = function () {
-
     return arguments[0];
-
 };
 
 var add = function (a, b) {
     return a + b;
 };
 
-var lazy_sum = make_lazy(add, 2, 3);
+//var lazy_sum = make_lazy(add, 2, 3);
 
-console.log(lazy_sum(2, 3));
+console.log(make_lazy(add)(2,3));
 
 var double = function (n) {
     return n * 2;
