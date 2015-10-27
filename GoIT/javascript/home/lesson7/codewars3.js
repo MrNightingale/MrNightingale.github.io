7cecd80b093ed0ae2eb4ea7b8c103951e956e10e
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-10-27 23:36:51 +0200

    fix lesson7 codewars3 task

--- GoIT/javascript/home/lesson7/codewars3.js
+++ GoIT/javascript/home/lesson7/codewars3.js
@@ -2,16 +2,24 @@
  * Created by Solovyov on 23.10.2015.
  */
 var make_lazy = function () {
-    return arguments[0];
+
+    var args = [].slice.call(arguments);
+
+    var operation = args.shift();
+
+    return function() {
+        return operation.apply(this, args);
+    };
+
 };
 
 var add = function (a, b) {
     return a + b;
 };
 
-//var lazy_sum = make_lazy(add, 2, 3);
+var lazy_sum = make_lazy(add, 2, 3);
 
-console.log(make_lazy(add)(2,3));
+console.log(lazy_sum());
 
 var double = function (n) {
     return n * 2;
@@ -19,4 +27,4 @@ var double = function (n) {
 
 var lazy_value = make_lazy(double, 5);
 
-console.log(lazy_value(5));
+console.log(lazy_value());
