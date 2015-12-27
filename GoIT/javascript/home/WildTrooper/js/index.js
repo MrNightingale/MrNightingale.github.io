8418b5e295509760c591e477d22927b03256982a
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-12-27 18:34:46 +0200

    update images

--- GoIT/javascript/home/WildTrooper/js/index.js
+++ GoIT/javascript/home/WildTrooper/js/index.js
@@ -26,7 +26,7 @@ var headerUser = page.querySelector('.header__user');
 
 // trooper Start BGPosition and step
 var trPos = 0;
-var trPosStep = 300;
+var trPosStep = 297;
 
 animateTrooper();
 
@@ -34,57 +34,64 @@ function animateTrooper() {
 
     // Show trooper
     setTimeout(function() {
+
         audio.src = 'media/intro.mp3';
+
         spanPCTime.innerHTML = pcTime.toFixed(3) + ' sec';
+
         game.appendChild(trooper);
+
         trooper.style.backgroundImage = 'url(images/trooper' + randomTrooper() + '.png)';
-        trooper.style.right = '0';
-        trooper.style.width = '160px';
+        trooper.style.right = '-75px';
+        trooper.style.width = '230px';
         trPos = 0;
         trooper.style.backgroundPosition = trPos + 'px';
+
     }, 1000);
 
     //  First Step
     setTimeout(function() {
-        trooper.style.right = '125px';
+        trooper.style.right = '50px';
         trPos -= trPosStep;
         trooper.style.backgroundPosition = trPos + 'px';
     }, 2000);
 
     //  Second Step
     setTimeout(function() {
-        trooper.style.right = '250px';
+        trooper.style.right = '150px';
         trPos -= trPosStep;
         trooper.style.backgroundPosition = trPos + 'px';
     }, 3000);
 
     //  Center
     setTimeout(function() {
-        trooper.style.right = '275px';
+        trooper.style.right = '200px';
         trPos -= trPosStep;
         trooper.style.backgroundPosition = trPos + 'px';
     }, 4000);
 
 }
 
+// Starting point
 var startTime;
 
 showFire();
 
 function showFire() {
 
-    // Fire
     setTimeout(function() {
+
+        audio.src = 'media/fire.m4a';
 
         fire.className = 'fire';
         fire.innerHTML = 'FIRE !!!';
 
         game.appendChild(fire);
 
+        trooper.style.right = '250px';
+        trooper.style.width = '150px';
         trPos -= trPosStep;
         trooper.style.backgroundPosition = trPos + 'px';
-
-        audio.src = 'media/fire.m4a';
 
         trooper.addEventListener('click', shoot);
 
@@ -94,34 +101,11 @@ function showFire() {
 
 }
 
-// Win and Loose windows
-var overlayWin = document.createElement('div');
-overlayWin.className = 'overlay-win';
-
-var overlayLoose = document.createElement('div');
-overlayLoose.className = 'overlay-lose';
-
-//Result info
-var resInfo = document.createElement('div');
-resInfo.className = 'result-info';
-
-// Play again button
-var btnPlay = document.createElement('button');
-btnPlay.className = 'btn-play';
-btnPlay.innerHTML = 'Play again <i class="fa fa-repeat"></i>';
-
-// Reward
-var rewardValue = 0;
-var reward = page.querySelector('.js-reward');
-
-btnPlay.addEventListener('click', again);
-
 function shoot() {
 
     setTimeout(function() {
         audio.src = 'media/shot.mp3';
     }, 4);
-
 
     fire.parentNode.removeChild(fire);
 
@@ -142,8 +126,6 @@ function shoot() {
 
         page.appendChild(overlayWin);
 
-//        resInfo.style.height = '75px';
-//        resInfo.style.paddingTop = '35px';
         resInfo.innerHTML = 'winner!';
         game.appendChild(resInfo);
 
@@ -206,6 +188,28 @@ function randomTrooper() {
     return Math.ceil(Math.random() * troopers.length);
 
 }
+
+// Win and Lose windows
+var overlayWin = document.createElement('div');
+overlayWin.className = 'overlay-win';
+
+var overlayLoose = document.createElement('div');
+overlayLoose.className = 'overlay-lose';
+
+//Result info
+var resInfo = document.createElement('div');
+resInfo.className = 'result-info';
+
+// Play again button
+var btnPlay = document.createElement('button');
+btnPlay.className = 'btn-play';
+btnPlay.innerHTML = 'Play again <i class="fa fa-repeat"></i>';
+
+// Reward
+var rewardValue = 0;
+var reward = page.querySelector('.js-reward');
+
+btnPlay.addEventListener('click', again);
 
 function again() {
     window.location.reload();
