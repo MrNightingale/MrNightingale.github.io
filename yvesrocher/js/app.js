9cf473ccd2d852135a0c8f3f59052307fbe342c7
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2016-07-20 11:57:46 +0300

    update js

--- yvesrocher/js/app.js
+++ yvesrocher/js/app.js
@@ -1,81 +1,11 @@
 $(function() {
-    $('.care__triangle-color1').hover(function() {
-      $('.care__img1').hide();
-      $('.care__header1').hide();
-      $('.care__triangle-color1').text('-');
+    $('.care__triangle-color').hover(function() {
+      $(this).prev().prev().hide();
+      $(this).prev().prev().prev().hide();
+      $(this).text('-');
     }, function() {
-      $('.care__img1').show();
-      $('.care__header1').show();
-      $('.care__triangle-color1').text('+');
-    });
-
-    $('.care__triangle-color2').hover(function() {
-        $('.care__img2').hide();
-        $('.care__header2').hide();
-        $('.care__triangle-color2').text('-');
-    }, function() {
-        $('.care__img2').show();
-        $('.care__header2').show();
-        $('.care__triangle-color2').text('+');
-    });
-
-    $('.care__triangle-color3').hover(function() {
-        $('.care__img3').hide();
-        $('.care__header3').hide();
-        $('.care__triangle-color3').text('-');
-    }, function() {
-        $('.care__img3').show();
-        $('.care__header3').show();
-        $('.care__triangle-color3').text('+');
-    });
-
-    $('.care__triangle-color4').hover(function() {
-        $('.care__img4').hide();
-        $('.care__header4').hide();
-        $('.care__triangle-color4').text('-');
-    }, function() {
-        $('.care__img4').show();
-        $('.care__header4').show();
-        $('.care__triangle-color4').text('+');
-    });
-
-    $('.care__triangle-color5').hover(function() {
-        $('.care__img5').hide();
-        $('.care__header5').hide();
-        $('.care__triangle-color5').text('-');
-    }, function() {
-        $('.care__img5').show();
-        $('.care__header5').show();
-        $('.care__triangle-color5').text('+');
-    });
-
-    $('.care__triangle-color6').hover(function() {
-        $('.care__img6').hide();
-        $('.care__header6').hide();
-        $('.care__triangle-color6').text('-');
-    }, function() {
-        $('.care__img6').show();
-        $('.care__header6').show();
-        $('.care__triangle-color6').text('+');
-    });
-
-    $('.care__triangle-color7').hover(function() {
-        $('.care__img7').hide();
-        $('.care__header7').hide();
-        $('.care__triangle-color7').text('-');
-    }, function() {
-        $('.care__img7').show();
-        $('.care__header7').show();
-        $('.care__triangle-color7').text('+');
-    });
-
-    $('.care__triangle-color8').hover(function() {
-        $('.care__img8').hide();
-        $('.care__header8').hide();
-        $('.care__triangle-color8').text('-');
-    }, function() {
-        $('.care__img8').show();
-        $('.care__header8').show();
-        $('.care__triangle-color8').text('+');
+        $(this).prev().prev().show();
+        $(this).prev().prev().prev().show();
+        $(this).text('+');
     });
 });
